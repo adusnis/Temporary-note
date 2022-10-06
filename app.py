0b7e12d684b9abad74a5ac5dab13c4f0397ac5d7
sinsuda <maewang.bigcnan444@gmail.com>
2022-10-06 23:37:42 +0700

    intitial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    f = open("note.txt", "r")
+    text = f.read()
+    return render_template('index.html',note = text)
+
+@app.route("/", methods=['POST'])
+def save():
+    textinput = dict(request.form.items())
+    input = textinput['inputsave']
+    with open("note.txt", mode='w', encoding='utf-8') as f:
+        f.write(f'{input}')
+    return render_template('index.html', note = input)
+
+
+app.run(debug=True)
