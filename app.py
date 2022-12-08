c62333715c543b0b2383274e8e675be01448379f
sinsuda <maewang.bigcnan444@gmail.com>
2022-12-08 22:31:35 +0700

    update

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/see', methods=['POST'])
-def see():
-    code = request.form['code']
+'''
+@app.route("/update")
+def update():
+    textinput = dict(request.form.items())
+    code = textinput['code']
     try:
         with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
             text = f.read()
     except:
-        return render_template('see.html', respond = "มีบางอย่างผิดพลาด")
+        return render_template('index.html', respond = "มีบางอย่างผิดพลาด")
+    return render_template('see.html', code =  code, text = text)
+'''
+@app.route('/see', methods=['POST'])
+def see():
+    textinput = dict(request.form.items())
+    code = textinput['code']
+    try:
+        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
+            text = f.read()
+    except:
+        return render_template('index.html', respond = "มีบางอย่างผิดพลาด")
     return render_template('see.html', code =  code, text = text)
 
 
 @app.route("/", methods=['POST'])
 def save():
     textinput = dict(request.form.items())
-    input = textinput['input-field']
+    text = textinput['input-field']
     code = ''.join(random.choices(string.ascii_letters, k=3))
     with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
         f.write(f'{input}')
-    return render_template('index.html', text = input, code = code)
+    return render_template('see.html', code=code, text=text)
