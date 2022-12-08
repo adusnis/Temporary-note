52a2aa102454ce4606690108091ee7a9fea065c1
sinsuda <maewang.bigcnan444@gmail.com>
2022-12-08 23:23:25 +0700

    update

--- app.py
+++ app.py
@@ -7,19 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-'''
-@app.route("/update")
-def update():
-    textinput = dict(request.form.items())
-    code = textinput['code']
-    try:
-        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
-            text = f.read()
-    except:
-        return render_template('index.html', respond = "มีบางอย่างผิดพลาด")
-    return render_template('see.html', code =  code, text = text)
-'''
-@app.route('/see', methods=['POST'])
+
+
+@app.route('/join', methods=['POST'])
 def see():
     textinput = dict(request.form.items())
     code = textinput['code']
@@ -31,11 +21,22 @@ def see():
     return render_template('see.html', code =  code, text = text)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/create", methods=['POST'])
 def save():
     textinput = dict(request.form.items())
     text = textinput['input-field']
     code = ''.join(random.choices(string.ascii_letters, k=3))
     with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
-        f.write(f'{input}')
+        f.write(f'{text}')
     return render_template('see.html', code=code, text=text)
+
+
+@app.route("/update", methods=['POST'])
+def update():
+    textinput = dict(request.form.items())
+    text = textinput['input-field']
+    code = textinput['code']
+    with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
+        f.write(f'{text}')
+    return render_template('see.html', code=code, text=text)
+app.run(debug=True)
