13ff66e0ff947145181a3760b6838fffaf78a273
sinsuda <maewang.bigcnan444@gmail.com>
2022-12-08 22:12:27 +0700

    update

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template, request
-
+import random
+import string
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    f = open("note.txt", "r")
-    text = f.read()
-    return render_template('index.html',note = text)
+    return render_template('index.html')
+
+@app.route('/see', methods=['POST'])
+def see():
+    code = request.form['code']
+    try:
+        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
+            text = f.read()
+    except:
+        return render_template('see.html', respond = "มีบางอย่างผิดพลาด")
+    return render_template('see.html', code =  code, text = text)
+
 
 @app.route("/", methods=['POST'])
 def save():
     textinput = dict(request.form.items())
-    input = textinput['inputsave']
-    with open("note.txt", mode='w', encoding='utf-8') as f:
+    input = textinput['input-field']
+    code = ''.join(random.choices(string.ascii_letters, k=3))
+    with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
         f.write(f'{input}')
-    return render_template('index.html', note = input)
+    return render_template('index.html', text = input, code = code)
 
+app.run(debug=True)
