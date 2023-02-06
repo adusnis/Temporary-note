49d9a0157e7a2035bc5d06edcc7d3e0aa722e5c0
sinsuda <maewang.bigcnan444@gmail.com>
2023-02-06 22:32:29 +0700

    update

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+import os
 from flask import Flask, render_template, request
 import random
 import string
 app = Flask(__name__)
+# define the path of folder storing text files
+# in this case, the path is root directory.
+
+DATA_PATH = "/"
 
 def createcode():
     code = ''.join(random.choices(string.ascii_letters, k=3))
-    try:
-        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
-            f.read()
-    except IOError:
-        return code
-    else:
+    if f"{code}.txt" in os.listdir(DATA_PATH):
+        # regenerate code
+        # actually, it is recommended to prevent similar code since the code came from random.
         return createcode()
-
+    return code
 
 @app.route('/')
 def index():
@@ -23,7 +25,7 @@ def see():
     textinput = dict(request.form.items())
     code = textinput['code']
     try:
-        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
+        with open(f'data/{code}.txt', mode='r', encoding='utf-8') as f:
             text = f.read()
     except:
         return render_template('index.html', respond = "มีบางอย่างผิดพลาด ไม่พบข้อมูล")
@@ -34,7 +36,7 @@ def see():
 def create():
     code = createcode()
     text = request.form.get('input-field')
-    with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
+    with open(f'data/{code}.txt', mode='w', encoding='utf-8') as f:
         f.write(text)
     print("มีคนมาใช้งานด้วยแหละแกร")
     return render_template('see.html', code=code, text=text)
@@ -43,7 +45,7 @@ def create():
 @app.route("/<code>/upload", methods=['POST'])
 def upload(code):
     text = request.form.get('input-field')
-    with open(f'{code}.txt', mode='w', encoding='utf-8') as f:
+    with open(f'data/{code}.txt', mode='w', encoding='utf-8') as f:
         f.write(text)
     return render_template('see.html', code=code, text=text)
     
@@ -52,10 +54,10 @@ def upload(code):
 @app.route('/<code>/update', methods=['POST'])
 def update(code):
     try:
-        with open(f'{code}.txt', mode='r', encoding='utf-8') as f:
+        with open(f'data/{code}.txt', mode='r', encoding='utf-8') as f:
             text = f.read()
     except:
         return render_template('see.html', respond="มีบางอย่างผิดพลาด ไม่พบข้อมูล", text=text)
     return render_template('see.html', code=code, text=text)
 
-#app.run(debug=True)
+app.run(debug=True)
